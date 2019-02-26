53814d07d74ac7499fce7ec7756f1e5c3916346d
cernanb <github email address>
2019-02-26 09:15:39 -0800

    Add faker gem with seed data for listings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+20.times do 
+    Listing.create(name: Faker::Company.name, price: Faker::Number.decimal(2), address: Faker::Address.full_address)
+end
